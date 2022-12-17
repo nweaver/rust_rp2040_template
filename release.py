27f9c702eb4a4682ab5034cb3b37b0ccd94bdaac
Nicholas Weaver <nweaver@gmail.com>
2022-12-17 12:08:04 -0800

    Checkpoint: set up to auto-run onto the RP Pico from Raspberry Pi setup

--- release.py
+++ release.py
@@ -3,7 +3,11 @@ import sys
 
 
 
-interface = "interface/picoprobe.cfg"
+#-f target/rp2040.cfg -c "program hello_serial.elf verify reset exit"
+
+
+#interface = "interface/picoprobe.cfg"
+interface = "interface/raspberrypi-swd.cfg"
 target = "target/rp2040.cfg"
 program = f'-c "program {sys.argv[1]} verify reset exit"'
 cmd = f'-f {interface} -f {target} {program}'
